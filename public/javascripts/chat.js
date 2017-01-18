7095c06044f492cd839c1d9ce8369c78f8b902a4
thandon263 <ncubethando20@gmail.com>
2017-01-18 00:44:30 -0500

    Creating the processing chat room commands

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,6 +2,8 @@ const Chat = (socket) => {
   this.socket = socket;
 };
 
+// Sending messages
+
 Chat.prototype.sendMessage = function (room, text) {
   let message = {
     room: room,
@@ -10,8 +12,41 @@ Chat.prototype.sendMessage = function (room, text) {
   this.socket.emit('message', message);
 };
 
+// Change rooms
+
 Chat.prototype.changeRoom = function (room) {
   this.socket.emit('join', {
     newRoom: room
   });
+};
+
+// Processing chat commands
+
+Chat.prototype.processCommand = function (command) {
+  let words = command.split(' ');
+  let command = words[0]
+                    .substring(1, words[0].length)
+                    .toLowerCase();
+  let message = false;
+
+  switch (command) {
+    case 'join':
+      words.shift();
+      let room = words.join(' ');
+      this.changeRoom(room);
+      break;
+
+    case 'nick':
+      words.shift();
+      let name = words.join(' ');
+      this.socket.emit('nameAttempt', name);
+      break;
+
+    default:
+      message = 'Unrecognized command.';
+      break;
+  }
+
+  return message;
+
 };
